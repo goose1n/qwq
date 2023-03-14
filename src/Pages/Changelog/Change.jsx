0c121895726d32723fe82372321ac5d6e88ccd64
Nazima <nazimazamirkulova14@gmail.com>
2023-03-14 15:44:39 +0600

    modified:   index.html 	new file:   public/ImgProj/photos/change.jpg 	modified:   src/Pages/Changelog/Change.jsx 	modified:   src/Pages/Changelog/Change.module.css

--- src/Pages/Changelog/Change.jsx
+++ src/Pages/Changelog/Change.jsx
@@ -1,10 +1,27 @@
 import React from "react";
 import SubBlock from "../../UI/SubBlock/SubBlock";
+import styles from "./Change.module.css";
 
 const Change = () => {
   return (
-    <div>
-      Change <SubBlock />
+    <div className={styles.Changecont}>
+      <div className={styles.ChangeBlock}>
+        <h1 className={styles.ChangeBlock_title}>Changelog</h1>
+        <img
+          className={styles.ChangeImg}
+          src="./public/ImgProj/banners/lic1.png"
+          alt=""
+        />
+      </div>
+      <div className={styles.change}>
+        <img src="./public/ImgProj/photos/change.jpg" alt="" />
+        <h1 className={styles.v}>V.1</h1>
+        <p className={styles.ChangeP}>
+          Initial Organick Webflow Template Release
+        </p>
+      </div>
+
+      <SubBlock />
     </div>
   );
 };
