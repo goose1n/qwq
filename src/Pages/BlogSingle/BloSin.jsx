730b44b08345f2758d0a7f0eceba77ac28e83d0f
ms-oblique22я <unknownfamous2021@gmail.com>
2023-03-15 23:26:55 +0400

    modified:   src/Pages/Blog/Blog.module.css     modified:   src/Pages/Blog/BlogCard/BlogCard.module.css     modified:   src/Pages/BlogSingle/BloSin.jsx     modified:   src/Pages/BlogSingle/BloSin.module.css

--- src/Pages/BlogSingle/BloSin.jsx
+++ src/Pages/BlogSingle/BloSin.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import styles from './BloSin.module.css'
 import SubBlock from "../../UI/SubBlock/SubBlock";
 
 const BloSin = () => {
   return (
     <div>
-      <p>hello world</p> 
+      <div className={styles.bannerBloSin3}>
+      <h1>hello</h1>
+      </div> 
+
+      <div className="subtitle">
+        <h3>Make perfect organic product with us</h3>
+        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking a layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
+        
+      </div>
       <SubBlock />
     </div>
   );
