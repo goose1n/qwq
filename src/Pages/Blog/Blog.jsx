25e9d1bb3c4dffa09f319b07033eb8b68bd8047c
ms-oblique22я <unknownfamous2021@gmail.com>
2023-03-22 18:40:25 +0400

    new file:   public/ImgProj/banners/bcg3.jpg 	new file:   public/ImgProj/banners/bcgBlogSingle3.png 	new file:   public/ImgProj/banners/bcgsinblog2.png 	modified:   src/App.jsx 	modified:   src/Pages/Blog/Blog.jsx 	modified:   src/Pages/Blog/BlogCard/BlogCard.jsx 	modified:   src/Pages/Blog/BlogCard/BlogCard.module.css 	deleted:    src/Pages/BlogSingle/BloSin.jsx 	deleted:    src/Pages/BlogSingle/BloSin.module.css 	new file:   src/Pages/BlogSingle3/BlogSingle3.jsx 	new file:   src/Pages/BlogSingle3/BlogSingle3.module.css

--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -13,12 +13,12 @@ const Blog = () => {
 
       <div className={styles.cards}>
         <div className={styles.grids}>
-          <BlogCard img="./public/ImgProj/photos/blogRoccah.png" name="By Rachi Card" title="The Benefits of Vitamin D & How to Get It" text='Simply dummy text of the printing and typesetting industry. Lorem Ipsum'></BlogCard>
-          <BlogCard img="./public/ImgProj/photos/blogTomato.png" name="By Rachi Card" title='Our Favorite Summertime Tomato' text='Simply dummy text of the printing and typesetting industry. Lorem Ipsum'></BlogCard>
-          <BlogCard img="./public/ImgProj/photos//blogVeggie.png" name="By Rachi Card" title='Benefits of Vitamin C & How to Get It' text='Simply dummy text of the printing and typesetting industry. Lorem Ipsum'></BlogCard>
-          <BlogCard img="./public/ImgProj/photos/blogMan.png" name="By Rachi Card" title='Research More Organic Foods' text='Simply dummy text of the printing and typesetting industry. Lorem Ipsum'></BlogCard>
-          <BlogCard img="./public/ImgProj/photos/blogLady.png" name="By Rachi Card" title='Research More Organic Foods' text='Simply dummy text of the printing and typesetting industry. Lorem Ipsum'></BlogCard>
-          <BlogCard img="./public/ImgProj/photos/blogTrash.png" name="By Rachi Card" title='Don’t Use Plastic Product! it’s Kill Nature' text='Simply dummy text of the printing and typesetting industry. Lorem Ipsum'></BlogCard>
+          <BlogCard img="./public/ImgProj/photos/blogRoccah.png" name="By Rachi Card" title="The Benefits of Vitamin D & How to Get It" text='Simply dummy text of the printing and typesetting industry. Lorem Ipsum' to='/BlogSingle1'></BlogCard>
+          <BlogCard img="./public/ImgProj/photos/blogTomato.png" name="By Rachi Card" title='Our Favorite Summertime Tomato' text='Simply dummy text of the printing and typesetting industry. Lorem Ipsum' to='/BlogSingle2'></BlogCard>
+          <BlogCard img="./public/ImgProj/photos/blogMan.png" name="By Rachi Card" title='Research More Organic Foods' text='Simply dummy text of the printing and typesetting industry. Lorem Ipsum' to='/BlogSingle3'></BlogCard>
+          <BlogCard img="./public/ImgProj/photos//blogVeggie.png" name="By Rachi Card" title='Benefits of Vitamin C & How to Get It' text='Simply dummy text of the printing and typesetting industry. Lorem Ipsum' to='/BlogSingle4'></BlogCard>
+          <BlogCard img="./public/ImgProj/photos/blogLady.png" name="By Rachi Card" title='Research More Organic Foods' text='Simply dummy text of the printing and typesetting industry. Lorem Ipsum' to='/BlogSingle5'></BlogCard>
+          <BlogCard img="./public/ImgProj/photos/blogTrash.png" name="By Rachi Card" title='Don’t Use Plastic Product! it’s Kill Nature' text='Simply dummy text of the printing and typesetting industry. Lorem Ipsum' to='/BlogSingle6'></BlogCard>
         </div>
       </div>
 
