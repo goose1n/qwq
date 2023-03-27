bdc156559840ddc031c8799341fce8d669a4d8f3
goose1n <danilsev07t@gmail.com>
2023-03-27 15:42:14 +0600

    modified:   src/Components/Nav/Nav.module.css 	modified:   src/Pages/BlogSingle1/BlogSingle1.module.css 	modified:   src/Pages/Licenses/Licenses.jsx 	modified:   src/Pages/Licenses/Licenses.module.css 	modified:   src/UI/PeopleCard/PeopleCard.jsx 	deleted:    src/UI/ShopCards/ShopCard.jsx

--- src/Pages/Shop/Shop.jsx
+++ src/Pages/Shop/Shop.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import ShopCard from "../../UI/ShopCards/Shop_Card";
 import SubBlock from "../../UI/SubBlock/SubBlock";
 import styles from './Shop.module.css'
 
 const Shop = () => {
   return (
     <div>
-      
+      <ShopCard/>
        <SubBlock />
     </div>
   );
