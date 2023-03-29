6fd3a80aeb0d77d7be317a255413727c9db71106
goose1n <danilsev07t@gmail.com>
2023-03-29 16:10:07 +0600

    new file:   public/ImgProj/banners/Patterns.png 	new file:   public/ImgProj/banners/bgcPortfolio.png 	new file:   public/ImgProj/icon/Icon.png 	modified:   src/Pages/Portfolio/Portfolio.jsx 	modified:   src/Pages/Portfolio/Portfolio.module.css 	modified:   src/UI/BlogCard/BlogCard.module.css 	new file:   src/UI/PortfolioCard/PortfolioCard.module.css

--- src/Pages/Portfolio/Portfolio.jsx
+++ src/Pages/Portfolio/Portfolio.jsx
@@ -1,10 +1,49 @@
-import React from 'react'
-import SubBlock from '../../UI/SubBlock/SubBlock'
+import React from "react";
+import PortfolioCard from "../../UI/PortfolioCard/PortfolioCard";
+import SubBlock from "../../UI/SubBlock/SubBlock";
+import styles from "./Portfolio.module.css";
 
 const Portfolio = () => {
   return (
-    <div>Portfolio <SubBlock/></div>
-  )
-}
+    <div className={styles.porCont}>
+      <div className={styles.banner}>
+        <h1 className={styles.bannerTitle}>Portfolio Standart</h1>
+      </div>
+      <div className={styles.grid}>
+        <PortfolioCard
+          img=".\public\ImgProj\photos\cardLemon.png"
+          name="Green & Tasty Lemon"
+          work="Fruits"
+        />
+        <PortfolioCard
+          img=".\public\ImgProj\photos\cardCarrot.png"
+          name="Organic Carrot"
+          work="Farmer"
+        />
+        <PortfolioCard
+          img=".\public\ImgProj\photos\cardBasil.png"
+          name="Organic Betel Leaf"
+          work="Leaf"
+        />
+        <PortfolioCard
+          img=".\public\ImgProj\photos\cardTomato.png"
+          name="Natural Tommato"
+          work="Fruits"
+        />
+        <PortfolioCard
+          img=".\public\ImgProj\photos\cardBlackberry.png"
+          name="Black Raspberry"
+          work="Farmer"
+        />
+        <PortfolioCard
+          img=".\public\ImgProj\photos\cardLemons.png"
+          name="Honey Orange"
+          work="Farmer"
+        />
+      </div>
+      <SubBlock />
+    </div>
+  );
+};
 
-export default Portfolio
+export default Portfolio;
