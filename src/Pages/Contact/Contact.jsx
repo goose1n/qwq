ef0daf39f38ed408a15ec907ee35432b695e4e80
Nazima <nazimazamirkulova14@gmail.com>
2023-04-03 14:20:09 +0600

    modified:   src/Pages/Contact/Contact.jsx

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -129,7 +129,7 @@ const Contact = () => {
        <div className={styles.input_block}>
        <div className={styles.Contact_MassegeInput}>
         <h1>Message*</h1>
-        <input type="text" placeholder="hello there,i would like to talk about how to..."  className={styles.inputmass}/>
+        <input type="text" placeholder="Hello there,i would like to talk about how to..."  className={styles.inputmass}/>
        </div>
        </div>
        <div className={styles.butt}>
