cade31a48dae9754a72692e55b70b004435791bb
goose1n <danilsev07t@gmail.com>
2023-04-04 11:16:36 +0600

    modified:   src/UI/YellowBtn/YellowBtn.jsx 	modified:   src/Pages/Home/Home.jsx 	modified:   src/Pages/ShopSingle/ShopSingle.jsx

--- src/UI/YellowBtn/YellowBtn.jsx
+++ src/UI/YellowBtn/YellowBtn.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import styles from './YellowBtn.module.css'
+import React from "react";
+import styles from "./YellowBtn.module.css";
+import { NavLink } from "react-router-dom";
 
-const YellowBtn = ({text}) => {
+const YellowBtn = ({ text, to }) => {
   return (
     <div className={styles.btnCont}>
-    <button className={styles.btn}>{text}<span><img src="./public\ImgProj\icon\Aerrow.png" alt="123" /></span></button>
+      <NavLink to={to}>
+        <button className={styles.btn}>
+          {text}
+          <span>
+            <img src="./public\ImgProj\icon\Aerrow.png" alt="123" />
+          </span>
+        </button>
+      </NavLink>
     </div>
-  )
-}
+  );
+};
 
-export default YellowBtn
+export default YellowBtn;
